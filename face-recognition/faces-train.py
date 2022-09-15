6d093c816fa8c1418d420e6ee308f5436f3c5794
Jorge Estrada <jorge_a.estrada@hotmail.com>
2022-09-15 14:13:48 -0500

    mayor changes

--- face-recognition/faces-train.py
+++ face-recognition/faces-train.py
@@ -4,14 +4,16 @@ import pickle
 import numpy as np
 from PIL import Image
 
-#OS.WALK FOR IMAGE FINDING
+# OS.WALK FOR IMAGE FINDING
 
-#Absolute path and images path for search of image files
+# Absolute path and images path for search of image files
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-img_dir = os.path.join(BASE_DIR, "images")
+IMG_DIR = os.path.join(BASE_DIR, "images")
+CASCADE_CLASSIFIER_DIR = os.path.join(
+    BASE_DIR, "data/haarcascades/haarcascade_frontalface_alt2.xml")
 
-#HAAR cascade clasifier xml, choose anyone of cascades/data/___.xml directory
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+# HAAR cascade clasifier xml, choose anyone of cascades/data/___.xml directory
+face_cascade = cv2.CascadeClassifier(CASCADE_CLASSIFIER_DIR)
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
@@ -19,49 +21,51 @@ label_ids = {}
 y_labels = []
 x_train = []
 
-#Find all the directories of the images contained in the root path
-for root, dirs, files in os.walk(img_dir):
-	for file in files:
-		if file.endswith("png") or file.endswith("jpg"):
-			path = os.path.join(root, file)
-			
-			#Labels from directories
-			label = os.path.basename(root).replace(" ", "-").lower()
-			#print(label, path)
+# Find all the directories of the images contained in the root path
+for root, dirs, files in os.walk(IMG_DIR):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
 
-			#Creating training labels
-			if not label in label_ids:
-				label_ids[label] = current_id
-				current_id += 1
+            # Labels from directories
+            label = os.path.basename(root).replace(" ", "-").lower()
+            #print(label, path)
 
-			id_ = label_ids[label]
-			print(label_ids)
+            # Creating training labels
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
 
-			#y_labels.append(label) #Some number
-			#x_train.append(path) #verify this image, turn into a NUMPY array gray
-			pil_image = Image.open(path).convert("L") #Grayscale
-			#Resize images for training (avoids the loss of accurate)
-			size = (550, 550)
-			final_image = pil_image.resize(size, Image.ANTIALIAS)
+            id_ = label_ids[label]
 
-			#Training data into a numpy array
-			img_array = np.array(final_image, "uint8")
-			#print("Image xD")
-			#print(img_array)
+            # y_labels.append(label) #Some number
+            # x_train.append(path) #verify this image, turn into a NUMPY array gray
+            pil_image = Image.open(path).convert("L")  # Grayscale
+            # Resize images for training (avoids the loss of accurate)
+            size = (550, 550)
+            final_image = pil_image.resize(size, Image.ANTIALIAS)
 
-			#Region of interest (ROI) in training data
-			faces = face_cascade.detectMultiScale(img_array, scaleFactor=1.5, minNeighbors=5)
+            # Training data into a numpy array
+            img_array = np.array(final_image, "uint8")
+            #print("Image xD")
+            # print(img_array)
 
-			for (x, y, w, h) in faces:
-				roi_image = img_array[y:y+h, x:x+w]
-				x_train.append(roi_image)
-				y_labels.append(id_)
+            # Region of interest (ROI) in training data
+            faces = face_cascade.detectMultiScale(
+                img_array, scaleFactor=1.5, minNeighbors=5)
 
-#Using PICKLE to save label ID's
+            for (x, y, w, h) in faces:
+                roi_image = img_array[y:y+h, x:x+w]
+                x_train.append(roi_image)
+                y_labels.append(id_)
+
+print(label_ids)
+
+# Using PICKLE to save label ID's
 with open("labels.pickle", 'wb') as f:
-	pickle.dump(label_ids, f)
+    pickle.dump(label_ids, f)
 
-#Train the OPENCV PEOPLE RECOGNIZER
+# Train the OPENCV PEOPLE RECOGNIZER
 recognizer.train(x_train, np.array(y_labels))
 recognizer.save("trainner.yml")
-#Load names from PICKLE
+# Load names from PICKLE
